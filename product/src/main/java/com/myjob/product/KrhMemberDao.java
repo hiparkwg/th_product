d37cfd4e13efede70e13f8d341ae155057fb82bf
ryumm <smileryunhah@naver.com>
2024-05-28 09:58:46 +0900

    krh

---
+++ product/src/main/java/com/myjob/product/KrhMemberDao.java
@@ -0,0 +1,37 @@
+package com.myjob.product;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
+
+import com.myjob.product.mybatis.MyFactory;
+
+@Component
+public class KrhMemberDao {
+    SqlSession session;
+    KrhMemberDao dao;
+    
+    public ProductVo view(Integer sno){
+        ProductVo vo=null;
+        SqlSession session = new MyFactory().getSession();
+        vo = session.selectOne("krhproduct.view",sno);
+        session.close();
+        //System.out.println(sno);
+        return vo;
+    }
+
+    public String delete(Integer sno){
+        SqlSession session = new MyFactory().getSession();
+        int cnt=session.delete("krhproduct.delete_product",sno);
+        String msg="";
+        if(cnt>0){
+            msg="축하합니다! 성공적으로 삭제되었습니다!";
+            session.commit();
+        }else{
+            msg="삐용삐용! 삭제에 실패하였습니다!";
+            session.rollback();
+        }
+        System.out.println(msg);
+        session.close();
+        return msg;
+    }
+}
---
+++ product/src/main/resources/static/js/krh_index.js
@@ -0,0 +1,37 @@
+$(function(){
+    list(); //list를 부를거야
+});
+
+let view = (sno)=> {
+    $.ajax({
+        url:"/krh_view",
+        type: "GET",
+        data: {"sno":sno},
+        success : (resp)=>{
+            let temp=$(resp).find(".view");
+            $('.center .left').html(temp);
+            viewEvent(sno);
+        }
+    })
+}
+
+function viewEvent(sno){
+    let btnDelete = document.querySelector(".btnDelete");
+
+    btnDelete.addEventListener('click',()=>{
+        let yn=confirm('정말 삭제하시겠습니까?');
+        if(!yn) return;
+
+        $.ajax({
+            url:"/deleteR",
+            type:"GET",
+            data:{"sno":sno},
+            success:(resp)=>{
+                list("")
+                    setTimeout(()=>{
+                        alert(resp)
+                    },200);
+            }
+        })
+    })
+}
