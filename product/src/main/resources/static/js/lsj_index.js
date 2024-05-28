ecb8edac30b4068cb9f94796b0d01bee0ec99fcb
sung <m9716@naver.com>
2024-05-28 10:53:45 +0900

    추가

---
+++ product/src/main/resources/static/js/lsj_index.js
@@ -0,0 +1,97 @@
+let findStr = document.querySelector(".findStr");
+let btnManage = document.querySelector(".btnManage");
+btnManage.onclick=()=>{
+   load();
+
+}
+let register = ()=>{
+
+    let btnRegister = document.querySelector(".btnRegister");
+    btnRegister.onclick=()=>{
+       
+        $.ajax({
+            url:"/register",
+            type:"GET",
+            success:(resp)=>{
+                let temp = $(resp).find(".register");
+                $(".left").html(temp);
+                let btnregisterR = document.querySelector(".btnregisterR");
+                btnregisterR.onclick=()=>{
+                    let temp = document.frmDisplay;
+                    let frm = $(temp).serialize();
+                    $.ajax({
+                        url:"/registerR",
+                        type:"POST",
+                        data:frm,
+                        success:(resp)=>{
+                            load();
+                        }
+                    })
+                }
+            }
+        })
+       
+    }
+    
+}
+let choiView = (code)=>{
+    $.ajax({
+        url:"/update",
+        type:"GET",
+        data:{"code":code},
+        success:(resp)=>{
+            let temp =$(resp).find(".update");
+            $(".left").html(temp);
+            let btnUpdateR = document.querySelector(".btnUpdateR");
+            btnUpdateR.onclick=()=>{
+                let ftemp = document.frmDisplay;
+                let ffrm = $(ftemp).serialize();
+
+                $.ajax({
+                    url:"/updateR",
+                    type:"POST",
+                    data:ffrm,
+                    success:(resp)=>{
+                        load();
+                    }
+                })
+            }
+        }
+
+    })
+}
+
+  
+
+let load=()=>{
+    let findStr="";
+    if(sessionStorage.getItem("findStr") != null){
+        findStr = sessionStorage.getItem("findStr");}
+    $.ajax({
+        url:"/view",
+        type:"GET",
+        success:(resp)=>{
+        let temp = $(resp).find(".view");
+        $(".left").html(temp);
+        }
+    })
+    
+    $.ajax({
+        url:"/list",
+        type:"GET",
+        data:{"findStr":""},
+        success:(resp)=>{
+            let temp = $(resp).find(".change");
+            $(".right").html(temp);
+            register();
+        }
+    })
+}
+
+
+
+
+
+
+
+
