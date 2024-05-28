2f380ae34562278a445ca19b80305e99cccbbdf2
TrialGame <bkh0708@naver.com>
2024-05-28 10:45:44 +0900

    수정

--- product/src/main/java/com/myjob/product/BjmProductController.java
+++ product/src/main/java/com/myjob/product/BjmProductController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class BjmProductController {
@@ -12,12 +11,6 @@ public class BjmProductController {
     @Autowired
     BjmProductDao dao;
     
-    @RequestMapping(path="/bjmRegister")
-    public ModelAndView bjmRegister(){
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("code/register");
-        return mv;
-    }
     @RequestMapping(path="/bjmRegisterR")
     public boolean bjmRegisterR(CodeVo vo){
         boolean b = false;
